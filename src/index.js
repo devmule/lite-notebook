272043508ebab5629027c0597963b6fac2721780
Maxim Batyatin <35449804+DevMule@users.noreply.github.com>
2021-02-24 00:55:44 +0500

    menu

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@
 // ================================================================================================================== //
 
 // renderMD - рендер разметки
-import {renderMD} from "./render/index.js";
+import {renderMD} from "./render";
 // pyWorker - python обработчик, работающий через web worker
 import {worker_api} from "./py_worker/worker_api.js";
 import elements from "./elements/index.js";
@@ -41,7 +41,7 @@ const lite_notebook = {
 	elements: elements,
 	
 	// methods
-	render_path: render_path,
+	// render_path: render_path,
 	load_hierarchy: load_hierarchy,
 	
 	// worker
@@ -54,6 +54,13 @@ window.lite_notebook = lite_notebook;
 async function render_path(path) {
 	let {node, root} = await get_hierarchy_node(path);
 	if (node) {
+		let menu_elements = document.getElementById("menu").getElementsByClassName("menu-elem");
+		for (let i = 0; i < menu_elements.length; i++) {
+			let el = menu_elements[i];
+			if (el.getAttribute("path") === path) el.classList.add("active");
+			else el.classList.remove("active");
+		}
+		
 		document.title = node.name;
 		
 		const url = window.location.href.split('?');
@@ -61,6 +68,7 @@ async function render_path(path) {
 		
 		lite_notebook.root = root;
 		lite_notebook.screen = document.getElementById("screen");
+		lite_notebook.screen.innerHTML = "";
 		
 		let md = await fetch(root + "/notebook");
 		let text = await md.text();
@@ -74,15 +82,20 @@ async function render_path(path) {
 }
 
 async function load_hierarchy(src) {
-	let hi_file = await fetch(src);
-	let hi_text = await hi_file.text();
+	let hierarchy_file = await fetch(src);
+	let hierarchy_text = await hierarchy_file.text();
 	
-	if (hi_text) {
-		lite_notebook.hierarchy = JSON.parse(hi_text);
+	if (hierarchy_text) {
+		lite_notebook.hierarchy = JSON.parse(hierarchy_text);
 		
 		let url = new URL(window.location.href);
 		let root = url.searchParams.get('page') || "notebooks";
+		
+		await load_menu();
 		await render_path(root);
+		
+	} else {
+		throw new Error('Error. no hierarchy file!');
 	}
 }
 
@@ -105,4 +118,24 @@ async function get_hierarchy_node(root) {
 	}
 	
 	return {node, root: real_path.join('/')};
+}
+
+async function load_menu() {
+	let menu = document.getElementById("menu");
+	menu.innerHTML = "";
+	
+	let draw_hi = (node, depth, path) => {
+		let el = document.createElement("p");
+		el.style.paddingLeft = `${12 * depth}px`
+		el.innerText = node.name;
+		el.classList.add("menu-elem");
+		menu.appendChild(el);
+		
+		let notebook_path = path + node.src;
+		el.onclick = () => render_path(notebook_path);
+		el.setAttribute("path", notebook_path);
+		for (let i = 0; i < node.children.length; i++) draw_hi(node.children[i], depth + 1, notebook_path + "/");
+	}
+	
+	draw_hi(lite_notebook.hierarchy, 1, "");
 }
