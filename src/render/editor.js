a264ec2a293672320c5e0f468ee4aa450544c385
Maxim Batyatin <35449804+DevMule@users.noreply.github.com>
2021-03-01 13:37:41 +0500

    вторая статейка

--- src/render/editor.js
+++ src/render/editor.js
@@ -3,19 +3,21 @@ async function parse(ch, element) {
 	let el = document.createElement("div");
 	el.innerText = ch.content;
 	
+	let editor_index = null;
+	
 	for (let i = 0; i < ch.flags.length; i++) {
 		let flag = ch.flags[i];
 		
 		if (flag.startsWith("id")) {
 			let index = flag.indexOf("=");
 			if (index < 0) continue;
-			el.id = flag.slice(index + 1, flag.length).trim();
+			editor_index = el.id = flag.slice(index + 1, flag.length).trim();
 		}
 	}
 	
 	element.appendChild(el);
 	
-	const editor = ace.edit("editor");
+	const editor = ace.edit(editor_index);
 	editor.getSession().setMode("ace/mode/python");
 	editor.setOptions({maxLines: 60, minLines: 20});
 }
