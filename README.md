943d961ffe9d01320e0a5425337b81322eeecb09
Maxim Batyatin <35449804+DevMule@users.noreply.github.com>
2021-12-27 20:38:18 +0500

    delete src

--- README.md
+++
@@ -1,8 +0,0 @@
-# lite-notebook
-
-[> run](https://devmule.github.io/lite-notebook/dist/)
-
-### todo
-- [ ] use tdd (https://habr.com/ru/company/rambler_and_co/blog/278503/)
-- [ ] make up an application html file and use notebooks with iFrame + messages
-
--- src/client/index.js
+++
@@ -1,9 +0,0 @@
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-
-import './style/main.css';
-import {AppScreen} from "./screen";
-
-const screen = new AppScreen();
-
-document.body.appendChild(screen.element);
